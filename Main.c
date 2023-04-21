935deea77ae8fb260ccaf7e18fa915f37924059c
TigerClips1 <spongebob1966@proton.me>
2023-04-21 15:38:49 -0400

    C Course

---
+++ Main.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+//Shape
+
+/*int main()
+*{
+    *printf("    / |\n");
+    *printf("   /  |\n");
+    *printf("  /   |\n");
+    *printf(" /    |\n");
+    *printf("/____ \n");
+    *return 0;
+*}
+*/
+
+// Print hello world
+/*int main2()
+*{
+    *printf("Hello World \n");
+
+    *return 0;
+*}
+*/
+
+
+/*
+*variables
+*int main3()
+*{
+    *char characterName[] = "Tom";
+    *int Age = 67;
+    *printf("There once was a man named %s \n", characterName);
+    *printf("he was %d years old.\n", Age);
+    
+    *Age = 30;
+    *printf("He really liked the name %s\n", characterName);
+    *printf("but did not line being %d.\n", Age);
+    
+    *return 0;
+*}
+*/
+
+//Data Type
+
+/*int main4()
+*{
+    *int age = 40;
+    *double gpa = 40.0;
+    *char grade = 'A';
+    *char phrase [] = "Giraffe Academy";
+
+
+    *return 0;
+*}
+*/
+
+//printf basic
+
+/*int main()
+*{
+    *int favnum = 90;
+    *char myChar = "i"
+    *printf("Hello"/"World")
+    *printf("Hello WOrld")
+    *printf("My favorite number is %d", 500);
+    *printf("My favorite %s is %d", "number" , 500);
+    *printf("My favorite %s is %d", "number" , favnum);
+    *printf("My favorite %c is %d",  myChar , favnum);
+
+    *return 0;
+*}
+*/
+
+
+//TODO watch the video Timestamp 45:26 Working With Numbers
+//Working With Numbers
